d6c92aec95996fc59a098b66795352e059f82a21
nrs5 <156202774+nrs5@users.noreply.github.com>
2024-04-21 20:57:30 -0400

    initial commit

---
+++ register.js
@@ -0,0 +1,30 @@
+async function signup() {
+    let email = document.getElementById("email").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let customer = { email: email, username: username, password: password };
+
+    let request = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(customer)
+    };
+
+    try {
+        let response = await fetch("https://test3-latest-cni3.onrender.com", request);
+        if (response.ok) {
+            alert("Registration successful! You can now login.");
+            location.href = "login.html";
+        } else {
+            // Handle server errors or other non-successful responses
+            console.error("Registration failed:", response.statusText);
+            alert("Registration failed. Please try again.");
+        }
+    } catch (error) {
+        // Handle network errors
+        console.error("Network error:", error);
+        alert("An error occurred. Please try again later.");
+    }
+}
